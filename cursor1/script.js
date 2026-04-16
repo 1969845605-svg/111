d9131b5fe8f8660c9dab04423ed9c3facc77e506
Jia Ru <1969845605@qq.com>
2026-04-16 22:55:05 +0800

    Delete cursor1 directory

--- cursor1/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('heart').addEventListener('click', function () {
-  this.style.transform = 'scale(1.3)';
-  setTimeout(() => {
-    this.style.transform = '';
-  }, 200);
-});
